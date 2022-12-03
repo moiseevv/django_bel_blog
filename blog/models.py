9d2cfd48f5caf01e7f6cc6027a2e4b68c51c4e82
Vladimir Moiseev neo <lutii@ngs.ru>
2022-12-03 14:05:43 +0700

    table Posts

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=257)
+    title = models.CharField(max_length=257, verbose_name='Статья')
+    text = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    publish_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
