f835f0b3efb18b232133c34a2a4261baa0842e32
Vladimir Moiseev neo <lutii@ngs.ru>
2022-12-03 15:31:10 +0700

    page html Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import hello_world
+from blog.views import hello_world, post_list
 
 urlpatterns = [
     path('hello-world/', hello_world),
+    path('', post_list, name='post_list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
+from blog.models import Post
+
 
 def hello_world(request):
-
     return HttpResponse(f"<h1>Hi  {datetime.now()}</h1>")
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'items': posts}
+    return render(request, 'blog/post_list.html', context)
