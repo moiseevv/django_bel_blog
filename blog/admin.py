7803b8edd266672307a11625614e3f71132af81a
Vladimir Moiseev neo <lutii@ngs.ru>
2022-12-04 18:28:04 +0700

    add model Cat

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comments
-
+from .models import Post, Comments, Cat
 admin.site.register(Post)
 admin.site.register(Comments)
+admin.site.register(Cat)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class Cat(models.Model):
+    name = models.CharField(max_length=255, verbose_name="наименование категории")
+
 
 class Post(models.Model):
     title = models.CharField(max_length=257, verbose_name='Статья')
@@ -7,9 +10,11 @@ class Post(models.Model):
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     publish_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     publish = models.BooleanField(default=False)
+    cat = models.ForeignKey(Cat , on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title}"
+
 
 class Comments(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
